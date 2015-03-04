4cfed49f55f8a896585c15da35d0c63612e1d4bb
Amar Menezes <menezes.amar@gmail.com>
2015-03-04 22:57:30 +0000

    Simplified SimulationClock design

--- src/service/clock/SimulationClock.java
+++ src/service/clock/SimulationClock.java
@@ -2,16 +2,42 @@ package service.clock;
 
 import java.util.Observable;
 
-public class SimulationClock extends Observable{
+public class SimulationClock extends Observable implements Runnable{
 	private long currentTime;
 	public final static SimulationClock clock = new SimulationClock();
 	private long interval;
+	
+	private Thread systemClock;
+	private volatile boolean suspended = false;
+	private volatile boolean running = false;
 	
 	private SimulationClock()
 	{
 		this.currentTime = 0;
 		//AM > Time in ms between each clock tick
 		this.interval=1000;
+		systemClock = new Thread(this);
+	}
+	
+	public void run()
+	{
+		try
+		{
+			while(true)
+			{
+				Thread.sleep(clock.getInterval());
+				synchronized(this)
+				{
+					if(!suspended)
+					{
+						clock.incrementClock();
+					}
+				}
+			}
+		}
+		catch(InterruptedException e)
+		{
+		}
 	}
 	
 	public static SimulationClock getInstance()
@@ -43,5 +69,24 @@ public class SimulationClock extends Observable{
 	public long getInterval()
 	{
 		return interval;
+	}
+	
+	public synchronized void pauseClock()
+	{
+		this.suspended = true;
+	}
+	
+	public synchronized void resumeClock()
+	{
+		this.suspended = false;
+	}
+	
+	public synchronized void startClock()
+	{
+		if(!running)
+		{
+			running = true;
+			systemClock.start();
+		}
 	}
 }
