bf47dd52159066695cc165934eab79a56bb2b690
Mighty-Caterpillar <paul.orlean-taub@kcl.ac.uk>
2015-03-08 13:58:41 +0000

    Observable fix & Singleton.

--- src/service/SimulationClock.java
+++ src/service/SimulationClock.java
@@ -6,17 +6,23 @@ public class SimulationClock extends Observable {
     private long delayCurrent;      /* Current pause tracker. */
     private long tickRate;          /* Number of ticks per second. */
 
+    private static final SimulationClock instance = new SimulationClock();
+
     /* Wagwan. */
-    public SimulationClock() {
+    private SimulationClock() {
         this.delayTime = 0;
         this.tickRate  = 1;
         this.startTime = System.currentTimeMillis();
     }
 
     /* Constructor for custom tickrate. */
-    public SimulationClock(long tickRate) {
+    private SimulationClock(long tickRate) {
         this();
         this.tickRate = tickRate;
+    }
+
+    public static SimulationClock getInstance() {
+        return this.instance;
     }
 
     public void resetClock() {
@@ -34,6 +40,7 @@ public class SimulationClock extends Observable {
 
     public void incrementClock() {
         this.startTime -= (1000 / this.tickRate);
+        setChanged();
         notifyObservers();
     }
 
