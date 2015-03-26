0318d05fe43708c740372d08154548e5cea7e870
KC <paul.orlean-taub@kcl.ac.uk>
2015-03-26 15:05:14 +0000

    Renderer

--- src/client/Renderer.java
+++ src/client/Renderer.java
@@ -15,15 +15,15 @@ public class Renderer
 		SOUTH,
 		WEST
 	}
-	
+
 	public static void renderRoad(Graphics g, int x, int y, int length, int width, Direction direction)
 	{
-		int offsetX = 0;
-		int offsetY = 0;
-		int blockX  = 0;
-		int blockY  = 0;
+		int offsetX    = 0;
+		int offsetY    = 0;
+		int blockX     = 0;
+		int blockY     = 0;
 		int blockWidth = width / 2;
-		
+
 		switch(direction) {
 		case NORTH:
 			y -= length;
@@ -38,26 +38,37 @@ public class Renderer
 			blockX = blockWidth;
 			offsetX = length;
 			offsetY = width;
-			break;		
+			break;
 		default:
 			break;
 		}
-		
-		
+
+
 		/* Render Ends */
-		g.setColor(Color.BLACK);
+		g.setColor(Color.GRAY);
 		g.fillRect(x - blockX, y - blockY, offsetX + blockX*2, offsetY + blockY*2);
-		
+
 		/* Render Road. */
 		Color old = g.getColor();
-		g.setColor(Color.GRAY);
+		g.setColor(Color.BLACK);
 		g.fillRect(x, y, offsetX, offsetY);
-		
-		/* Render White. */
+
+		/* Render Divider. */
 		g.setColor(Color.WHITE);
-		g.fillRect(x, y, offsetX, offsetY);
-		g.drawLine(x + 30, y + 30, offsetX, offsetY);
-		
+		g.drawLine(x + blockY, y + blockX, x + offsetX - blockY, y + offsetY - blockX);
+
+		/* Render Stripes. */
+		Graphics2D g2d = (Graphics2D) g.create();
+		g2d.setStroke(new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0));
+		g2d.drawLine(x + blockY + (blockY / 2), y + blockX + (blockX / 2), x + offsetX - (blockY / 2), y + offsetY - (blockX / 2));
+		g2d.drawLine(x + blockY - (blockY / 2), y + blockX - (blockX / 2), x + offsetX - 3 * (blockY / 2), y + offsetY - 3 * (blockX / 2));
+
+		/* Render Letters. */
+		g.setColor(Color.WHITE);
+		g.drawString("A", x + blockY - (blockX / 2), y + blockX - (blockY / 2));
+		g.drawString("B", x + offsetX + blockY + (blockX / 2), y + offsetY - blockX - (blockY / 2));
+
+		/* Render Letters. */
 		g.setColor(old);
 	}
 }
